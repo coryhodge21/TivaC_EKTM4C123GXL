7a38c9cd47d89c6036516eddcfb3f518ed501a68
Cory Hodge <coryhodge@Corys-MBP.attlocal.net>
2020-12-15 21:29:00 -0500

    stoping development on this framework now that i have tested some theories and have a seemingly strong approach. Pausing to write C program to parse mem maps and produce library of APIs for this chip. Developer notes in main

--- Framework_v_0_1/main.c
+++ Framework_v_0_1/main.c
@@ -4,6 +4,30 @@
  * \date 12/14/2020
  *
  * driver for mcu framework
+ *
+ *
+ * TODO
+ * 12/15/2020 Next Steps:
+ *
+ * test struct base address typedefs
+ *  either volatile unisinged int * or just unsigned int
+ *
+ * then finish writing MODULE.h with proper regArr[REG_ARR_SIZE] or use
+ * enums like register.h
+ *
+ * module will be optional in future, currently independant of register
+ *
+ * goal is to use syntax : Module.Register.action(Bit_Field);
+ * while also keeping memory space in mind
+ *
+ * I will write a C program to read the ti provided mem maps for each
+ * module/register. It will produce a library of files I refer to as
+ * the FrameWork. Each one will mimick the files in this project, being:
+ * Register.h
+ * Register.c
+ * Module.h
+ * Module.c (to create upon return)
+ *
  */
 
 // Project Common Files
